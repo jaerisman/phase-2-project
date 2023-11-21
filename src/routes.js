745547b4662b2d1056f890d21e09cd392a81b81c
Jacob Erisman <jacob.erisman@gmail.com>
2023-11-21 18:35:30 -0500

    add routes.js file

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,23 @@
 import Home from "./components/Home";
+import AllHikes from "./components/AllHikes"
+import CompletedHikes from "./components/CompletedHikes"
 
 const routes = [
   {
     path: "/",
     element: <Home />,
     errorElement: <ErrorPage />
-  }
+  },
+  {
+    path: "/",
+    element: <AllHikes />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/",
+    element: <CompletedHikes />,
+    errorElement: <ErrorPage />
+  },
 ];
 
 export default routes;
