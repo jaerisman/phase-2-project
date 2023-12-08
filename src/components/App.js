6707ee906477eed2ae4f4a44b5ca14872c1dd8ed
Jacob Erisman <jacob.erisman@gmail.com>
2023-12-08 09:14:17 -0500

    edit app component

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import NavBar from "./NavBar";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
