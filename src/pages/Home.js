0fc1ae6ade50a2dfcfbcb9609441bf5857051ffa
Jacob Erisman <jacob.erisman@gmail.com>
2023-12-08 02:37:42 -0500

    add background image styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,15 +3,17 @@ import NavBar from "../components/NavBar";
 
 function Home(){
     return (
-        <>
+        <div className="home">
+          <div className="home-image" />
           <header>
             <Header />
+            <p>Welcome to Hiking CO! Learn about some of Colorado's great hikes, or add your own hikes. Happy hiking!</p>
             <NavBar />
           </header>
           <main>
-            <p>Welcome to Hiking CO! Learn about some of Colorado's great hikes, add your own hikes, and track the ones you've completed. Happy hiking!</p>
+            <p>Welcome to Hiking CO! Learn about some of Colorado's great hikes, or add your own hikes. Happy hiking!</p>
           </main>
-        </>
+        </div>
     );
 };
 
